163584d49bebc9a571ee3a62e0daccb69a6e2f3d
Anna Musial <ania.musial13@gmail.com>
2018-11-03 18:31:44 +0100

    Created template for products list

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
     <nav-bar></nav-bar>
+    <h1>What's your style?</h1>
+    <products-list></products-list>
     <el-row :gutter="20">
       <el-col :span="12">
       </el-col>
