fc73ae79ca7693d3fdefc3ae5f30f0685980c464
Anna Musial <ania.musial13@gmail.com>
2018-11-09 16:20:45 +0100

    Added About view

---
+++ src/Components/About.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="about">
+    <h1>{{ title }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: 'About us'
+    }
+  }
+}
+</script>
